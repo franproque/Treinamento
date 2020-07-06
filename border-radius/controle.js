c4e900922ac97eac5accee9fcf18d9715bce686a
franproque <franproque@gmail.com>
2020-07-06 20:57:04 -0300

    border-radius finalizado

--- border-radius/controle.js
+++ border-radius/controle.js
@@ -1,7 +1,27 @@
 function pegaValoresCampo(){
-    var esquerdaTop= document.getElementsByName("esquerda-top");
+    
+var esquerdaTop= document.querySelector("#esquerda-top").value;
+var direitaTop= document.querySelector("#direita-top").value;
 
-   console.log(esquerdaTop.values())
+var esquerdaBaixo= document.querySelector("#esquerda-baixo").value;
+var direitaBaixo= document.querySelector("#direita-baixo").value;
+var valorDoCSS= esquerdaTop+"px "+direitaTop+"px "+ direitaBaixo+"px "+esquerdaBaixo+"px";
+
+document.querySelector("#centro").style.borderRadius =valorDoCSS;
+
+
+
+document.querySelector("#imprimir").innerHTML="border-radius: "+esquerdaTop+"px "+direitaTop+"px "+ direitaBaixo+"px "+esquerdaBaixo+"px;";
+console.log(direitaBaixo)
+}
+function limpa(){
+    document.querySelector("#imprimir").innerHTML=" ";
+
+}
+
+function copia(){
+    var copia = document.querySelector("#imprimir").value;
+    document.execCommand("copy");
 }
 
 
