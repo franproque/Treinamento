022dbcbbf4db33cfbe08fd49c31f4e1cd6dcdeff
franproque <franproque@gmail.com>
2020-07-10 10:51:42 -0300

    Gerador de nick quase pronto

---
+++ CPF-Generator/geradorDeNickRPG.js
@@ -0,0 +1,47 @@
+
+function quantidadeDeLetras(){
+  var quantidade=0;  
+while(quantidade<=2){
+    quantidade=Math.floor(Math.random()*10);
+}
+
+return quantidade;
+    
+
+
+}
+function escolheLetra(min,max){
+
+return Math.floor(Math.random()*(max-min) +min)
+
+
+}
+function letrasAleatorias() {
+    const alfabeto=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    const nick=[]
+    for(i=0; i<quantidadeDeLetras();i++){
+
+        
+        nick.push(alfabeto[escolheLetra(0,25)]);
+        
+    }
+
+    var vogais=[alfabeto[0],alfabeto[4],alfabeto[8],alfabeto[14],alfabeto[20]]
+
+    var nickPronto="";
+    for(x=0;x<nick.length; x++){
+        for(z=0;z<vogais.length;z++){
+
+            if(nick[x]===vogais[z]){
+                console.log("Iguais")
+            }else{
+                nickPronto=nickPronto+nick[x];
+                console.log(nickPronto)
+            }
+        }
+        
+    }
+   console.log(nickPronto)
+}
+
+letrasAleatorias();
